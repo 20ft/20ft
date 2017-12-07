e82e0a5bd85b39a7e75906cb8e650cc453268a25
David Preece <davep@zedkep.com>
2017-12-07 14:10:34 +1300

    Added DKIM

--- tfnz/components/mail/__init__.py
+++ tfnz/components/mail/__init__.py
@@ -11,12 +11,19 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 import re
+from datetime import date
 from tfnz.location import Location, RankBias
 from tfnz.volume import Volume
 
+dkim_template = '''
+    dkim_domain = %s
+    dkim_selector = %s
+    dkim_private_key = /var/mail/dkim.key.pem
+'''
+
 
 class Mail:
-    def __init__(self, domain_name: str, recipients: [str], ssh=False):
+    def __init__(self, domain_name: str, recipients: [str], *, dkim=False, ssh=False, image=None):
         # check the recipients are in name:pass format
         recipient_re = re.compile('\A[a-zA-Z0-9_.+-]+:\S+\Z')
         self.recipients = [r.strip('\n') for r in recipients]
@@ -27,11 +34,16 @@ class Mail:
             raise ValueError("Exim needs a domain name to function (try 'local'?)")
         self.domain_name = domain_name
         self.ssh = ssh
+        self.server_condition = '${if !eq{$tls_cipher}{}}'  # nasty hack to get around .format trying to expand it
+        self.dkim = dkim
+        self.dkim_text = ''
+        self.image = 'tfnz/mail' if image is None else image
 
-    def go(self, location: Location, volume: Volume, *, log_callback=None, local_smtp=25, local_imap=993):
+    def go(self, location: Location, volume: Volume, *, log_callback=None,
+           local_smtp=25, local_smtps=465, local_imap=993):
         # create the container
         node = location.ranked_nodes(RankBias.memory)[0]
-        container = node.spawn_container('tfnz/mail',
+        container = node.spawn_container(self.image,
                                          volumes=[(volume, '/var/mail')],
                                          termination_callback=location.disconnect,
                                          sleep=True).wait_until_ready()
@@ -43,8 +55,26 @@ class Mail:
         # create certs if not there
         if b'server.pem' not in container.run_process('ls /var/mail')[0]:
             container.run_process('openssl req -x509 -newkey rsa:1024 -sha256 '
-                                  '-keyout /var/mail/server.key -out /var/mail/server.pem -nodes -days 365 '
+                                  '-keyout /var/mail/server.key -out /var/mail/server.pem -nodes -days 9999 '
                                   '-subj \'/CN=%s\'' % self.domain_name)
+
+        if self.dkim:
+            # same again for dkim
+            if b'dkim.key.pem' not in container.run_process('ls /var/mail')[0]:
+                container.run_process('openssl genrsa -out /var/mail/dkim.key.pem 1024 -outform PEM')
+                container.run_process('openssl rsa -in /var/mail/dkim.key.pem'
+                                      '            -out /var/mail/dkim.pub.pem -pubout -outform PEM')
+                container.put('/var/mail/dkim_selector', date.today().strftime('%Y%M%d').encode())
+
+            # render the dkim configuration
+            dkim_selector = container.fetch('/var/mail/dkim_selector')
+            dkim_pk = container.fetch('/var/mail/dkim.pub.pem')
+            self.dkim_text = dkim_template % (self.domain_name, dkim_selector.decode())
+
+            # log the DNS changes
+            print("@ TXT \"v=spf1 a mx ip4:this.is.mx.ip\"")
+            print("%s._domainkey TXT \"v=DKIM1; p=%s\"" %
+                  (dkim_selector.decode(), dkim_pk.decode()[27:-26].replace('\n', '')))
 
         # create user accounts
         passwd_file = b''
@@ -62,9 +92,9 @@ class Mail:
         # attach an SMTP tunnel and run exim
         container.spawn_process('tail -f /var/log/exim/mainlog', data_callback=log_callback)
         container.attach_tunnel(25, localport=local_smtp)
+        container.attach_tunnel(465, localport=local_smtps)
         container.spawn_process('/usr/sbin/exim -bdf', data_callback=log_callback)  # creates the exim user
 
         # same again with dovecot
-        container.spawn_process('tail -f /var/log/dovecot.log', data_callback=log_callback)
         container.attach_tunnel(993, localport=local_imap)
         container.spawn_process('/usr/sbin/dovecot', data_callback=log_callback)
